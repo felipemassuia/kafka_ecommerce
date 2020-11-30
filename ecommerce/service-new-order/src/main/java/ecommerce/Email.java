d02a116dd664c0b0075583035135de4890f7224a
Felipe Massuia <massuia98.felipe@gmail.com>
2020-11-29 22:00:39 -0300

    Modularizando o projeto

---
+++ ecommerce/service-new-order/src/main/java/ecommerce/Email.java
@@ -0,0 +1,14 @@
+package ecommerce;
+
+public class Email {
+	
+	private final String subject;
+	private final String body;
+	
+	public Email(String subject, String body) {
+		super();
+		this.subject = subject;
+		this.body = body;
+	}
+	
+}
