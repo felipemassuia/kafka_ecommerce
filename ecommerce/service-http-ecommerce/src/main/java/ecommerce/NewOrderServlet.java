7a570433d6c4a5d3c25790b35fa6d4fd097556dc
Felipe Massuia <massuia98.felipe@gmail.com>
2020-12-05 04:26:30 -0300

    Adicionando indepotencia na nova ordem

--- ecommerce/service-http-ecommerce/src/main/java/ecommerce/NewOrderServlet.java
+++ ecommerce/service-http-ecommerce/src/main/java/ecommerce/NewOrderServlet.java
@@ -1,10 +1,5 @@
 package ecommerce;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,46 +7,57 @@ import javax.servlet.http.HttpServletResponse;
 
 import ecommerce.dispatcher.KafkaDispatcher;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+
 public class NewOrderServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	
 	private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-	
+
 	@Override
 	public void destroy() {
 		super.destroy();
 		try {
+			
 			orderDispatcher.close();
+			
 		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 		try {
 
+			// we are not caring about any security issues, we are only
+			// showing how to use http as a starting point
 			var email = req.getParameter("email");
-			var orderId = UUID.randomUUID().toString();
 			var amount = new BigDecimal(req.getParameter("amount"));
-
+			var orderId = req.getParameter("uuid");
 			var order = new Order(orderId, amount, email);
 
-			orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, new CorrelationId(NewOrderServlet.class.getSimpleName()), order);
+			try (var database = new OrdersDatabase()) {
+				if (database.saveNew(order)) {
+					orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
+							new CorrelationId(NewOrderServlet.class.getSimpleName()), order);
 
-			System.out.println("New order sent successfully.");
+					System.out.println("New order sent successfully.");
+					resp.setStatus(HttpServletResponse.SC_OK);
+					resp.getWriter().println("New order sent");
+				} else {
+					System.out.println("Old order received.");
+					resp.setStatus(HttpServletResponse.SC_OK);
+					resp.getWriter().println("Old order received");
+				}
+			}
 
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.getWriter().println("New order sent");
-
-		} catch (ExecutionException e) {
-			throw new ServletException();
-		} catch (InterruptedException e) {
-			throw new ServletException();
+		} catch (InterruptedException | SQLException | ExecutionException e) {
+			throw new ServletException(e);
 		}
 
 	}
-
 }
